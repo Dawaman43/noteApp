6371921d636384ca1afb2fe1cc3fd6738df9674f
Dawaman43 <dawitworkujima@gmail.com>
2025-02-19 07:35:27 -0300

    Added base view for root URL

--- noteApp/urls.py
+++ noteApp/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from note import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('note/', include('note.urls')),  # Include your app's URLs
-    path('accounts/', include('django.contrib.auth.urls')),  # Include authentication URLs
+    path('note/', include('note.urls')),  
+    path('accounts/', include('django.contrib.auth.urls')),  
+    path('', views.base_view, name='base'),
 ]
